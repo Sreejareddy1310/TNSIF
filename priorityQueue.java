b0b65bb553cbb08bf53f7e6c577f057a3742b279
Sreejareddy1310 <ysreeja1310@gmail.com>
2025-05-22 12:55:30 +0530

    Add files via upload

---
+++ priorityQueue.java
@@ -0,0 +1,17 @@
+package com.TNSIF.CollectrionsTwo;
+
+//import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+public class Priority Queue {
+	public static void main(String [] args){
+		PriorityQueue <Integer> ll=new Priority Queue<Integer>();
+		ll.add(15);
+		ll.add(75);
+		ll.add(66);
+		ll.add(108);
+		for(int x:l5){
+			System.out.println(x);
+		}
+	}
+}
